c002530be139ebe7793059720436df88230762f4
nathancirca <74016600+nathancirca@users.noreply.github.com>
2022-06-25 22:48:55 +0200

    Delete test_yaml.py

--- test_yaml.py
+++
@@ -1,4 +0,0 @@
-import yaml
-dict_file=[{"names":["person" ,"cat"]}]
-with open("/home/pc-visualisation/Bureau/database_training/data.yaml", 'w') as file:
-    documents = yaml.dump(dict_file, file,default_flow_style=None)
